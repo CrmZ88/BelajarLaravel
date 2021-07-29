41985c033d167dc95d249ec866897f59faa2631b
Farras Wirawan <farraswirawan4874@gmail.com>
2021-07-29 16:31:31 +0700

    installationLaravel

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,42 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| This file is where you may define all of the routes that are handled
+| by your application. Just tell Laravel the URIs it should respond
+| to using a Closure or controller method. Build something great!
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Auth::routes();
+Route::get('/', 'HomeController@index');
+Route::get('/Listbook', 'GuestController@index');
+Route::get('/home', 'HomeController@index');
+Route::group(['prefix'=>'admin', 'middleware'=>['auth', 'role:admin']], function () {
+  Route::resource('authors', 'AuthorsController');
+  Route::resource('books', 'BooksController');
+  Route::get('export/books', [
+    'as'   => 'export.books',
+    'uses' => 'BooksController@export'
+  ]);
+  Route::post('export/books', [
+    'as'   => 'export.books.post',
+    'uses' => 'BooksController@exportPost'
+  ]);
 });
+
+Route::get('books/{book}/borrow', [
+  'middleware' => ['auth', 'role:member'],
+  'as'         => 'guest.books.borrow',
+  'uses'       => 'BooksController@borrow'
+]);
+
+Route::put('books/{book}/return', [
+  'middleware' => ['auth', 'role:member'],
+  'as'         => 'member.books.return',
+  'uses'       => 'BooksController@returnBack'
+]);
+
